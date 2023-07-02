2706e1dd6d883befc27a03062c69a7f518bba40e
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-07-02 16:56:40 +0800

    feat: add database validators

--- index.js
+++ index.js
@@ -62,6 +62,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -133,7 +135,7 @@ const generateId = () => {
   return maxId
 }
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   // if (!body.content) {
@@ -156,9 +158,12 @@ app.post('/api/notes', (request, response) => {
 
   /* notes = notes.concat(note)
   response.json(note) */
-  note.save().then(savedNote => {
-    response.json(savedNote)
-  })
+  note
+    .save()
+    .then(savedNote => {
+      response.json(savedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -169,7 +174,11 @@ app.put('/api/notes/:id', (request, response, next) => {
     important: body.important,
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, note, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
     .then(updatedNote => {
       response.json(updatedNote)
     })
