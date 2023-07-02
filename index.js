f9003a7fca1e45f5f531faaab09f777ffde93428
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-07-02 15:16:35 +0800

    feat: add POST endpoint notes route

--- index.js
+++ index.js
@@ -161,7 +161,22 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.put('/api/notes/:id', (request, response) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+/* app.delete('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   notes = notes.filter(note => note.id !== id)
 
