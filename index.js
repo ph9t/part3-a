2137cae27468c8a6096aff28cac8fe1e34f3e829
Angel Jimenez <58505078+ph9t@users.noreply.github.com>
2023-07-02 15:29:17 +0800

    fix: correct returned status and data

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.json(400).send({ error: 'malformatted id' })
+    return response.status(400).send({ error: 'malformatted id' })
   }
 
   next(error)
